e3d65bbce53a7388648a5ef4996ac3de9a61d04c
cgnerds <cgnerds@gmail.com>
2018-09-14 17:41:45 +0800

    C++ test plugin.

---
+++ Assets/AREngine/PluginWrappers/NativeWindowsWrapper.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class NativeWindowsWrapper : MonoBehaviour {
+	[DllImport("NativeWindowsPlugin")]
+	public static extern int Addify(int a, int b);
+	
+	private void Start() {
+		var add = Addify(2, 4);
+		Debug.Log(add);
+	}
+}
