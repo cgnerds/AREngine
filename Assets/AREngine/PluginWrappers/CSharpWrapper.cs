c34b5df565e60e6256203280711c837cb89bb334
cgnerds <cgnerds@gmail.com>
2018-09-14 14:31:53 +0800

    C# Test Plugin.

---
+++ Assets/AREngine/PluginWrappers/CSharpWrapper.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class CSharpWrapper : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        var addition = new CSharpManagedPlugin.Addition();
+        var add = addition.Addify(5, 2);
+        Debug.Log(add);
+    }
+}
