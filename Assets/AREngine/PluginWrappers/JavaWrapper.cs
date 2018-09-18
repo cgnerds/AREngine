030ba5c996ec6dfbcb5286dba1d2be191f6a6580
cgnerds <cgnerds@gmail.com>
2018-09-18 21:58:15 +0800

    Java test plugin.

---
+++ Assets/AREngine/PluginWrappers/JavaWrapper.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JavaWrapper : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		#if UNITY_ANDROID && !UNITY_EDITOR
+		    var javaClass = new AndroidJavaObject("com.casia.androidlibrary.Addition");
+			int add = javaClass.Call<int>("Addify", 2, 2);
+			Debug.Log(add);
+		#endif
+	}
+}
