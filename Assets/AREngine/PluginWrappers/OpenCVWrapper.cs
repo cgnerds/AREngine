3e41db9d5685ff2483d22dddc12f256e1a7d4682
cgnerds <cgnerds@gmail.com>
2018-09-20 17:07:10 +0800

    OpenCV Test plugin.

---
+++ Assets/AREngine/PluginWrappers/OpenCVWrapper.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.InteropServices;
+internal static class OpenCVWrapper {
+	[DllImport("OpenCVPlugin")]
+	internal static extern int Init(ref int outCamereWidth, ref int outCameraHeight);
+	[DllImport("OpenCVPlugin")]
+	internal static extern int Close();
+	[DllImport("OpenCVPlugin")]
+	internal static extern void SetScale(int downscale);
+	[DllImport("OpenCVPlugin")]
+	internal unsafe static extern void Detect(CvCircle* outFaces, int maxOutFacesCount, ref int outDetectFacesCount);
+	
+	[StructLayout(LayoutKind.Sequential, Size=12)]
+	public struct CvCircle
+	{
+		public int X, Y, Radius;
+	}
+}
